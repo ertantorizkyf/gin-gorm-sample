572a4c8b88576a349ba210cd757e41a25e59dce0
Rizky Ertanto <ertantorizkyf@gmail.com>
2024-06-19 23:45:11 +0700

    Update init and endpoints

--- main.go
+++ main.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"github.com/ertantorizkyf/gin-gorm-sample/controllers"
 	"github.com/ertantorizkyf/gin-gorm-sample/initializers"
 	"github.com/gin-gonic/gin"
 )
 
 func init() {
 	initializers.LoadEnvVars()
+	initializers.ConnectDB()
 }
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	r.GET("/articles", controllers.ArticleIndex)
+	r.GET("/articles/:id", controllers.ArticleDetail)
+	r.POST("/articles", controllers.ArticleCreate)
+	r.PUT("/articles/:id", controllers.ArticleUpdate)
+	r.DELETE("/articles/:id", controllers.ArticleDelete)
+
 	r.Run()
 }
